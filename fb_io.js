f9e2329883ba30d0c5ff52348d221c2624254ca6
23046ys <23046ys@hvhs.school.nz>
2026-06-07 22:52:12 +0000

    Rules+Login

---
+++ fb_io.js
@@ -0,0 +1,42 @@
+var GLOBAL_user; 
+
+  var authenticationListener
+
+  function fb_readError(error) {
+    console.log("There was an error reading the message");
+    console.error(error);
+  }
+
+  function fb_login(){
+    authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+  }
+
+  function fb_handleLogin(_user) {
+    if (_user) {
+        console.log("User is logged in")
+        GLOBAL_user = _user;//Save the user details object to a global variable
+        console.log(GLOBAL_user.uid)
+        console.log(GLOBAL_user)
+    } else {
+        console.log("User is NOT logged in - Starting the popup process")
+        fb_popupLogin();
+    }
+
+}
+
+
+  function fb_popupLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    
+    
+    firebase.auth().signInWithPopup(provider).then((result) => {
+    GLOBAL_user = result.user;  // Save the user details object to a global variable
+    console.log("User has logged in")
+    });
+      console.log(GLOBAL_user.uid)
+      console.log(GLOBAL_user)
+
+    
+     
+  }
+
