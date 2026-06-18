809af7a7d66e7d724bc73d2b8bbbb0a6ad0945ec
23046ys <23046ys@hvhs.school.nz>
2026-06-18 21:51:38 +0000

    Highscores can be displayed now for both games

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ function submitDetails(){
 
      })
 }
+
+function fb_readHighscores(){
+   console.log("Reading Scores")
+   firebase.database().ref('/Users/').orderByValue().on('value', fb_displayHighscores, fb_readError);
+}
+
+function fb_displayHighscores(snapshot){
+   var dbData = snapshot.val();
+
+   const SCORES_ONE = document.getElementById("Game1Scores")
+   const SCORES_TWO = document.getElementById("Game2Scores")
+
+   if (dbData == null) { 
+        console.log('There was no record when trying to read the message');
+        
+    }else{
+      SCORES_ONE.innerHTML = "<p></p>"; 
+      SCORES_TWO.innerHTML = "<p></p>"; 
+       snapshot.forEach(fb_showScores)
+    }
+
+}
+
+function fb_showScores(child){
+   const SCORES_ONE = document.getElementById("Game1Scores")
+   const SCORES_TWO = document.getElementById("Game2Scores")
+
+   var gameOneScore = child.val()["Game1 Score"];
+   var gameTwoScore = child.val()["Game2 Score"];
+   
+   var gameName = child.val()["Game Name"];
+
+   SCORES_ONE.innerHTML += "<p>"+gameName+": "+gameOneScore+"</p>";
+   SCORES_TWO.innerHTML += "<p>"+gameName+": "+gameTwoScore+"</p>";
+}
+
+
+fb_readHighscores()
